0757ce027e6e0763797830f5cc8467f0ec81f042
aoztanir <aoztanir25@gmail.com>
2021-05-26 09:20:55 -0700

    hi

--- main.py
+++ main.py
@@ -1989,7 +1989,7 @@ class Music(commands.Cog, wavelink.WavelinkMixin):
       
       player.loopSong = not player.loopSong
       if player.loopSong==True:
-        if player.current!=None and len(player.queue)>=1:
+        if player.current!=None :
           ratio= fuzz.ratio(player.current.title, player.queue[0].title)
           if ratio<90:
             player.queue.insert(0,player.current)
